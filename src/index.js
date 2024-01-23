7fc472901e5f945f7362c9d23dd7fdafdcb48ded
Aboyeji Emmanuel <aboyejiemmanuel07@gmail.com>
2024-01-23 22:06:19 +0100

    sign up

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join('public')));
+
 app.get('/', (req, res) => {
-    res.render("login")
-})
+    res.render("login");
+});
 
 app.get('/signup', (req, res) => {
-    res.render("signup")
-})
-
+    res.render("signup");
+});
 
 const port = 5000;
-app.listen(port, () =>{
-    console.log(`Server Started on port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server Started on port ${port}`);
+});
